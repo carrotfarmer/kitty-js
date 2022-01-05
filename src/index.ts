67f50c07331625e2688d3bf44f91b64333086389
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-01-05 18:09:40 +0530

    move types to index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,45 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 import { BASE_URL, API_KEY, CDN_URL } from "./constants";
 
 // types
-import { Breed } from "./types";
+export interface Breed {
+  weight: {
+    imperial: string;
+    metric: string;
+  };
+  id: string;
+  name: string;
+  temperament: string;
+  origin: string;
+  country_code: string;
+  description: string;
+  life_span: string;
+  indoor: number;
+  alt_names: string;
+  adaptability: number;
+  affection_level: number;
+  child_friendly: number;
+  dog_friendly: number;
+  energy_level: number;
+  grooming: number;
+  health_issues: number;
+  intelligence: number;
+  shedding_level: number;
+  social_needs: number;
+  stranger_friendly: number;
+  vocalisation: number;
+  experimental: number;
+  hairless: number;
+  natural: number;
+  rare: number;
+  supressed_tail: number;
+  short_legs: number;
+  wikipedia_url: string;
+  hypoallergenic: number;
+  reference_image_id: string;
+  image: {
+    url: string;
+  };
+}
 
 /**
  * Retrieves the URL of a random cat image
